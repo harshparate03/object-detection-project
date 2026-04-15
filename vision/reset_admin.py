7428f9b662d58e047ccdbc540982d7bda6d1efc6
harshparate03 <harshparate03@gmail.com>
2026-04-15 16:18:52 +0530

    Fix reset_admin: just reset existing admin password

--- vision/reset_admin.py
+++ vision/reset_admin.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from vision.models import UserProfile
+from django.contrib.auth.hashers import make_password
 
 def reset_admin_password(request):
-    """Temporary view to reset admin password on Render - remove after use"""
     secret = request.GET.get('secret', '')
     if secret != 'vision_reset_2024':
         return HttpResponse('Unauthorized', status=403)
@@ -10,18 +10,7 @@ def reset_admin_password(request):
     try:
         admin = UserProfile.objects.filter(role='admin').first()
         if not admin:
-            admin = UserProfile(
-                username='admin',
-                email='admin@gmail.com',
-                name='Admin',
-                phone='0000000000',
-                role='admin',
-                is_superuser=True,
-                is_staff=True
-            )
-            admin.set_password('Admin@1234')
-            admin.save()
-            return HttpResponse('Admin created! Email: admin@gmail.com | Password: Admin@1234')
+            return HttpResponse('No admin found in database')
         
         admin.set_password('Admin@1234')
         admin.save()
